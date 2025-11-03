442bcf1ed368dd0dd3762bcdd23e5b43beaa2144
TommasiLarkingDavide <davide.tommasilarking@marconibari.edu.it>
2025-11-03 18:17:22 +0000

    DavideConsegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,13 +1,35 @@
 
 public class Esercizio {
 
-    public static void riempiPerRighe(/* scrivere qui i parametri formali */) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
-    }
+    public static void riempiRighe(int [][] M, int righe,int  colonne) {
+		int i,c,j;
+		c=1;
+		i=0;
+		while(i<=righe-1) {
+			j=0;
+			while(j<colonne-1) {
+				M[i][j]=c;
+				++c;
+				++j;
+			}
+			++i;
+		}
+	}
 
-    public static void riempiPerColonne(/* scrivere qui i parametri formali */) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
-    }
+  	public static void riempiColonne(int [][] M, int righe,int  colonne) {
+		int i,c,j;
+		c=1;
+		j=0;
+		while(j<=colonne-1) {
+			i=0;
+			while(i<righe-1) {
+				M[i][j]=c;
+				++c;
+				++i;
+			}
+			++j;
+		}
+	}
 
     public static void main(String[] args) {
         // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
